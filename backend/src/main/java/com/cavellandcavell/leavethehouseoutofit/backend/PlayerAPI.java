bdcea986a3422deaa5e4ef05a602673954c6fc16
Jonathan Cavell <Jonathan Cavell>
2017-06-19 08:11:42 -0400

    Implemented inital effort, for some reason I haven't figured out the login buttons are not showing up.  Will also still need to implement functionality for what to do if the user is already logged in.

--- backend/src/main/java/com/cavellandcavell/leavethehouseoutofit/backend/PlayerAPI.java
+++ backend/src/main/java/com/cavellandcavell/leavethehouseoutofit/backend/PlayerAPI.java
@@ -6,9 +6,14 @@
 
 package com.cavellandcavell.leavethehouseoutofit.backend;
 
+import com.google.api.server.spi.auth.EspAuthenticator;
 import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiIssuer;
+import com.google.api.server.spi.config.ApiIssuerAudience;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.response.UnauthorizedException;
 
 
 import javax.inject.Named;
@@ -21,7 +26,18 @@ import javax.inject.Named;
                 ownerDomain = "backend.leavethehouseoutofit.cavellandcavell.com",
                 ownerName = "backend.leavethehouseoutofit.cavellandcavell.com",
                 packagePath = ""
-        )
+        ),
+        authenticators = {EspAuthenticator.class},
+        issuers = {
+                @ApiIssuer(
+                      name = "firebase",
+                      issuer = "https://securetoken.google.com/lthoi-test",
+                      jwksUri = "https://www.googleapis.com/service_accounts/v1/metadata/x509/securetoken@system.gserviceaccount.com"
+                )
+        },
+        issuerAudiences = {
+                @ApiIssuerAudience(name = "firebase", audiences = "lthoi-test")
+        }
 )
 public class PlayerAPI {
 
@@ -29,11 +45,18 @@ public class PlayerAPI {
      * A simple endpoint method that takes a name and says Hi back
      */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi(@Named("name") String name, User u) throws UnauthorizedException{
+        if (u == null)
+        {
+            throw new UnauthorizedException("Invalid Credentials");
+        }
+
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("Hi, " + name + " " + u.getEmail());
 
         return response;
     }
+
+
 
 }
